a273b0f4922617ee43d975a947d483a1c9e10db6
Valon Pllana <44208145+ValonPllana@users.noreply.github.com>
2019-04-29 15:31:39 +0200

    .

---
+++ projekti/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace projekti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plaintexti = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnkripto = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDekripto = new System.Windows.Forms.Button();
+            this.textdekriptuar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keyword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ciphertext = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // plaintexti
+            // 
+            this.plaintexti.Location = new System.Drawing.Point(12, 47);
+            this.plaintexti.Name = "plaintexti";
+            this.plaintexti.Size = new System.Drawing.Size(729, 20);
+            this.plaintexti.TabIndex = 0;
+            this.plaintexti.TextChanged += new System.EventHandler(this.plaintexti_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plaintext";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnEnkripto
+            // 
+            this.btnEnkripto.Location = new System.Drawing.Point(566, 126);
+            this.btnEnkripto.Name = "btnEnkripto";
+            this.btnEnkripto.Size = new System.Drawing.Size(173, 41);
+            this.btnEnkripto.TabIndex = 2;
+            this.btnEnkripto.Text = "Enkripto";
+            this.btnEnkripto.UseVisualStyleBackColor = true;
+            this.btnEnkripto.Click += new System.EventHandler(this.btnEnkripto_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text i enkriptuar";
+            // 
+            // btnDekripto
+            // 
+            this.btnDekripto.Location = new System.Drawing.Point(572, 296);
+            this.btnDekripto.Name = "btnDekripto";
+            this.btnDekripto.Size = new System.Drawing.Size(169, 40);
+            this.btnDekripto.TabIndex = 5;
+            this.btnDekripto.Text = "Dekripto";
+            this.btnDekripto.UseVisualStyleBackColor = true;
+            this.btnDekripto.Click += new System.EventHandler(this.btnDekripto_Click);
+            // 
+            // textdekriptuar
+            // 
+            this.textdekriptuar.Location = new System.Drawing.Point(12, 359);
+            this.textdekriptuar.Name = "textdekriptuar";
+            this.textdekriptuar.Size = new System.Drawing.Size(728, 20);
+            this.textdekriptuar.TabIndex = 6;
+            this.textdekriptuar.TextChanged += new System.EventHandler(this.textdekriptuar_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 332);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Text i dekriptuar";
+            // 
+            // keyword
+            // 
+            this.keyword.Location = new System.Drawing.Point(481, 84);
+            this.keyword.Name = "keyword";
+            this.keyword.Size = new System.Drawing.Size(258, 20);
+            this.keyword.TabIndex = 8;
+            this.keyword.TextChanged += new System.EventHandler(this.keyword_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(424, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Key :";
+            // 
+            // ciphertext
+            // 
+            this.ciphertext.Location = new System.Drawing.Point(15, 223);
+            this.ciphertext.Name = "ciphertext";
+            this.ciphertext.Size = new System.Drawing.Size(724, 20);
+            this.ciphertext.TabIndex = 10;
+            this.ciphertext.TextChanged += new System.EventHandler(this.ciphertext_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ciphertext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.keyword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textdekriptuar);
+            this.Controls.Add(this.btnDekripto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEnkripto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plaintexti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox plaintexti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnkripto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDekripto;
+        private System.Windows.Forms.TextBox textdekriptuar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox keyword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ciphertext;
+    }
+}
+
---
+++ projekti/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projekti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private char[,] GjeneroMatricen(string keyword)
+        {
+            char[,] matrica = new char[5, 5];
+
+            StringBuilder sbAlfabeti = new StringBuilder();
+            for (int i = 65; i < 65 + 26; i++)
+                if ((char)i != 'I')
+                    sbAlfabeti.Append((char)i);
+
+            keyword = keyword.ToUpper();
+            keyword = keyword.Replace("I", "J");
+
+            for (int i = 0; i < keyword.Length; i++)
+                sbAlfabeti.Replace(keyword[i].ToString(), "");
+
+
+
+
+
+            int k = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    if (k < keyword.Length)
+                    {
+                        for (int l = 0; l < k; l++)
+                        {
+                            if (k < keyword.Length && keyword[l] == keyword[k])
+                            {
+                                k++;
+                                l = -1;
+                            }
+                        }
+                        if (k < keyword.Length) { 
+                        matrica[i, j] = keyword[k];
+                            k++;
+                        }
+                    }
+                    else
+                    {
+                        matrica[i, j] = sbAlfabeti[0];
+                        sbAlfabeti.Remove(0, 1);
+                    }
+                }
+            return matrica;
+        }
+
+        private string Enkripto(string plaintexti, char[,] matrica)
+        {
+            
+            StringBuilder sb = new StringBuilder("");
+            plaintexti = plaintexti.Replace("I", "J");
+            plaintexti = plaintexti.Replace(" ", "");
+
+            for (int i = 0; i < plaintexti.Length; i++)
+            {
+                char chA = plaintexti[i];
+                int pozitaX = 0, pozitaY = 0;
+                for (int j = 0; j < 5; j++)
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (matrica[j, k] == chA)
+                        {
+                            pozitaX = j+1;
+                            pozitaY = k+1;
+                        }
+                    }
+                
+                string enc = pozitaX.ToString() + pozitaY.ToString();
+                sb.Append(enc);
+            }
+
+            return sb.ToString();
+        }
+
+        private string Dekripto(string ciphertexti, char[,] matrica)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ciphertexti.Length; i += 2)
+            {
+                char enc11 = ciphertexti[i];
+                char enc21 = ciphertexti[i + 1];
+                int enc1 = int.Parse(enc11.ToString())-1;
+                int enc2 = int.Parse(enc21.ToString())-1;
+                char dec='w';
+                for (int j = 0; j < 5; j++)
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (j == enc1 && k == enc2 )
+                        {
+                            dec = matrica[j, k];
+                        }
+                        
+                    }
+                sb.Append(dec);
+            }
+
+            return sb.ToString();
+        }
+
+
+
+        private void plaintexti_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnkripto_Click(object sender, EventArgs e)
+        {
+            char[,] matrica = GjeneroMatricen(keyword.Text);
+            string ciphertexti = Enkripto(plaintexti.Text.ToUpper(), matrica);
+            ciphertext.Text = ciphertexti;
+        }
+        
+
+        private void keyword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDekripto_Click(object sender, EventArgs e)
+        {
+            char[,] matrica = GjeneroMatricen(keyword.Text);
+            string textDekriptuar = Dekripto(ciphertext.Text, matrica);
+            textdekriptuar.Text = textDekriptuar;
+        }
+
+        private void textdekriptuar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ciphertext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
